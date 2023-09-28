79e187ea7ad1c44c1d4efe2ccaa20771019b9706
Atul Sharma <atul.sharma.123647@gmail.com>
2023-09-29 04:07:46 +0530

    change contact.jsx

--- portfolio-Atul/src/components/Contact.jsx
+++ portfolio-Atul/src/components/Contact.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
+
 const gitFormEndpoint = import.meta.env.VITE_GITFORM_ENDPOINT;
 
 const Contact = () => {
+  const [data, setData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  console.log(data.email);
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
   return (
     <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="sm:text-4xl text-3xl font-bold text-center text-[#001b5e]">
@@ -21,6 +40,7 @@ const Contact = () => {
               name="name"
               id="name"
               className="border-2 rounded-lg p-3 flex border-gray-300"
+              onChange={handleOnChange}
             />
           </div>
           <div className="flex flex-col">
@@ -32,6 +52,7 @@ const Contact = () => {
               name="phone"
               id="phone"
               className="border-2 rounded-lg p-3 flex border-gray-300"
+              onChange={handleOnChange}
             />
           </div>
         </div>
@@ -44,6 +65,7 @@ const Contact = () => {
             name="email"
             id="email"
             className="border-2 rounded-lg p-3 flex border-gray-300"
+            onChange={handleOnChange}
           />
         </div>
         <div className="flex flex-col py-2">
@@ -55,6 +77,7 @@ const Contact = () => {
             name="subject"
             id="subject"
             className="border-2 rounded-lg p-3 flex border-gray-300"
+            onChange={handleOnChange}
           />
         </div>
         <div className="flex flex-col py-2">
@@ -66,6 +89,7 @@ const Contact = () => {
             rows={10}
             id="message"
             className="border-2 rounded-lg p-2 border-gray-300"
+            onChange={handleOnChange}
           />
         </div>
         <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-3 rounded-lg cursor-pointer">
